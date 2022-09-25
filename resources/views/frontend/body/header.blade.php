ccc222d19071c67744c87e48a154ca814a8c31cc
BoengGit <edukasiasqor@gmail.com>
2022-09-25 20:36:58 +0700

    setup-frontend

---
+++ resources/views/frontend/body/header.blade.php
@@ -0,0 +1,18 @@
+<header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+        <h1 class="logo"><a href="index.html">Dunclop<span>.</span></a></h1>
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                <li><a class="nav-link scrollto" href="#about">About</a></li>
+                <li><a class="nav-link scrollto" href="#services">Services</a></li>
+                <li><a class="nav-link scrollto" href="#contact">contact</a></li>
+                <li><button class="btn btn-primary" href="#contact">Login</button></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </div>
+</header>
---
+++ resources/views/frontend/body/hero.blade.php
@@ -0,0 +1,9 @@
+<section id="hero" class="d-flex align-items-center">
+    <div class="container" data-aos="zoom-out" data-aos-delay="100">
+      <h1 class="mb-3">Selamat Datang di <span>Dunclop</span></h1>
+      <h2 style="font-weight: 500">Memulai dari ruang yang gelap untuk menjadi ruang dengan hasil yang ceria</h2>
+      <div class="d-flex">
+        <a href="#about" class="btn-get-started scrollto">Get Started</a>
+      </div>
+    </div>
+  </section>
