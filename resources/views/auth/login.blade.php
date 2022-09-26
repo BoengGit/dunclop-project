fabff004b4851ff8afd22d860201fc65ebfa62bb
BoengGit <edukasiasqor@gmail.com>
2022-09-26 11:55:23 +0700

    auth-login-admin page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,14 +70,15 @@
               <!-- /Logo -->
               <h4 class="mb-2">Selamat datang di Dunclop! 👋</h4>
               <p class="mb-4">Tolong masukan akun admin anda untuk melanjutkan!</p>
-              <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
+              <form id="formAuthentication" class="mb-3" action="{{ route('login') }}" method="POST">
+                @csrf
                 <div class="mb-3">
                   <label for="email" class="form-label">Username</label>
                   <input
                     type="text"
                     class="form-control"
                     id="email"
-                    name="email-username"
+                    name="username"
                     placeholder="Enter your username"
                     autofocus
                   />
@@ -88,7 +89,7 @@
                   </div>
                   <div class="input-group input-group-merge">
                     <input
-                      type="password"
+                      type="text"
                       id="password"
                       class="form-control"
                       name="password"
@@ -96,12 +97,6 @@
                       aria-describedby="password"
                     />
                     <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                  </div>
-                </div>
-                <div class="mb-3">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="remember-me" />
-                    <label class="form-check-label" for="remember-me"> Remember Me </label>
                   </div>
                 </div>
                 <div class="mb-3">
